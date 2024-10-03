5ed97f33626e9e8325e325ede76700f05afc7ad9
lazer002 <ajlazer002@gmail.com>
2024-10-03 17:55:39 +0530

    api changes

--- router/router.js
+++ router/router.js
@@ -246,8 +246,6 @@ router.post('/signin', async (req, res) => {
 router.post('/searchfriend', authMiddleware, async (req, res) => {
   try {
     const { userkey } = req.body;
-    console.log('userkey: ', userkey);
-
     const data = await newuser.find({ 
       $or: [ 
         { username: { $regex: userkey, $options: 'i' } },  
@@ -270,12 +268,25 @@ router.post('/searchfriend', authMiddleware, async (req, res) => {
 // ################################## add friend ################################
 router.post('/addfriend', authMiddleware, async (req, res) => {
   try {
-    const { userId } = req.body;
+    const { userId, action } = req.body;
     const sessionUserId = req.user_id;
+
+    const existingRequest = await newuser.findOne({
+      user_id: userId,
+      'friend_requests.from_user': sessionUserId,
+    });
+
+    if (existingRequest) {
+      return res.status(400).json({ msg: 'already' });
+    }
+
+    const updateData = action === 'Add' 
+      ? { $addToSet: { friend_requests: { from_user: sessionUserId, status: 'pending' } } } 
+      : { $addToSet: { friend_requests: { from_user: sessionUserId, status: 'blocked' } } };
 
     const data = await newuser.findOneAndUpdate(
       { user_id: userId },
-      { $addToSet: { friend_requests: { from_user: sessionUserId, status: 'pending' } } },
+      updateData,
       { new: true }
     );
 
@@ -289,6 +300,7 @@ router.post('/addfriend', authMiddleware, async (req, res) => {
     return res.status(500).json({ msg: 'Server error' });
   }
 });
+
 
 
 
